e0a5fe81c1ecea32a269be166bbb77fe5c1e3ea2
Marek Hrabe <marekhrabe@me.com>
2017-06-02 15:09:33 +0200

    iframe communication

--- client/my-sites/preview/controller.js
+++ client/my-sites/preview/controller.js
@@ -3,37 +3,18 @@
  */
 // import ReactDom from 'react-dom';
 import React from 'react';
-import page from 'page';
 
 /**
  * Internal dependencies
  */
 import WebPreviewContent from 'components/web-preview/content';
 
-let initialLoad = true;
-
 export default {
 	preview: function( context, next ) {
-		if ( initialLoad ) {
-			initialLoad = false;
-			window.addEventListener( 'message', ( e ) => {
-				try {
-					const data = JSON.parse( e.data );
-					if ( data.channel === 'preview-asdf123' ) {
-						switch ( data.type ) {
-							case 'link':
-								page( data.payload.replace( 'https://wordpress.com', '' ) );
-								return;
-						}
-					}
-				} catch ( err ) {}
-			} );
-		}
-
 		context.primary = (
 			<div style={ { height: '100%' } }>
 				<WebPreviewContent
-					previewUrl={ `https://${ context.params.site }/?iframe=true&preview=true&calypso_token=asdf123` }
+					previewUrl={ `https://${ context.params.site }/?iframe=true&preview=true` }
 					showClose={ false }
 					loadingMessage="Beep beep poop…"
 				/>
